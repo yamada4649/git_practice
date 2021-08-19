9d0c6ea5f8318f532f4943b5fb713a56e5e09256
yamada4649 <ryuki.yamada4649@gmail.com>
2021-08-19 18:48:48 +0900

    user/bodyカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_082622) do
+ActiveRecord::Schema.define(version: 2021_08_19_094224) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_08_19_082622) do
     t.integer "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
